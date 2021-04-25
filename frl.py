6f2445e2136254d124525a4e60e27158054f6fa4
farrellhartomo <farrell.yodihartomo@gmail.com>
2021-04-26 06:37:29 +0700

    update baru

--- frl.py
+++ frl.py
@@ -1,37 +1,24 @@
-from kivymd.app import MDApp
+
 from kivy.lang import Builder
+
+from kivymd.app import MDApp
 from kivymd.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.widget import Widget
-
-import kivy.properties as KVprop 
-
-#error handling
-KVprop.ObjectProperty()
 
 from kivy.core.window import Window
 Window.size = (480,705)
 
-from login.login import loginApp
 from trip.trip import TripPage
+from login.login import LoginPage
 
-
-class ScreenLogin(Screen):
-    pass
-
-class ScreenTrip(Screen):
-    pass
-
-class MainScreen(ScreenManager):
-    screen_login = loginApp()
-    screen_trip = TripPage()
+class MainScreen(BoxLayout):
+    screen_log = LoginPage()
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.ids.scr_login.add_widget(self.screen_login)
-        #self.ids.scr_trip.add_widget(self.screen_trip)
+        self.ids.scr_login.add_widget(self.screen_log)
+        self.ids.scr_trip.add_widget(self.screen_trip)
 
 class frlApp(MDApp):
-    
     def build(self):
         self.theme_cls.primary_palette = "Teal"
         return MainScreen()
--- login/login.py
+++ login/login.py
@@ -1,32 +1,32 @@
-
 from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
+from kivymd.theming import ThemeManager
 
 from kivy.core.window import Window
 Window.size = (480,705)
 
-#Builder.load_file("login/login.kv")
+Builder.load_file("login/login.kv")
 
 class LoginPage(BoxLayout):
+    def verifyLogin(self):
+        info = self.ids.info_txt
+        username = self.ids.usr_txt.text
+        password = self.ids.usr_pwd.text
+
+        if username == "admin" and password == "admin":
+            info.text = '[color=#ffffff]logged in successfully![/color]'
+            self.parent.parent.current = 'scr_login'
+        else:
+            if username =='' and password == '':
+                info.text = '[color=#ffffff]Please enter username and password[/color]'
+            else:
+                info.text = '[color=#ffffff]Incorrect username or password[/color]'
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.verifyLogin()
 
-    def verifyLogin(self):
-        info = self.ids["info_txt"]
-        username = self.ids["usr_txt"].text
-        password = self.ids["usr_pwd"].text
-
-        if username == "admin" and password == "admin":
-            info.text = 'logged in successfully!'
-        else:
-            if username =='' and password == '':
-                info.text = 'Please enter username and password'
-            else:
-                info.text = 'Incorrect username or password'
-
-    
 class loginApp(MDApp):
     def build(self):
         return LoginPage()
