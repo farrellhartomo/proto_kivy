0e3c81afea1b24394230d1741b3aaf5542c5e5eb
farrellhartomo <farrell.yodihartomo@gmail.com>
2021-04-28 14:50:59 +0700

    add db and maps

--- db/sqldbinit.py
+++ db/sqldbinit.py
@@ -1,6 +1,17 @@
 import sqlite3
+from sqlite3 import Error
 
-conn = sqlite3.connect(':memory:')
+#set db default init
+db = sqlite3.connect(':memory:')
+
+conn = db.cursor()
+
+def db_conn():
+    try:
+        con = sqlite3.connect(':memory:')
+        return con
+    except Error:
+        print(Error)
 
 class create_tables:
     #create user table (lookup)
@@ -14,8 +25,11 @@ class create_tables:
         (
             city_id INTEGER PRIMARY KEY AUTOINCREMENT,
             city_name TEXT,
-            city_desc TEXT
-        )""")
+            city_desc TEXT,
+            city_lon FLOAT,
+            city_lat FLOAT
+            CHECK (city_lon >= -180 AND city_lon < 181 AND city_lat >= -90 AND city_lat < 91)
+            )""")
 
     #create user trip (transaction)
     conn.execute("""CREATE TABLE tripdb
@@ -27,8 +41,17 @@ class create_tables:
             FOREIGN KEY (fk_city_id) REFERENCES citydb(city_id)
 
         )""")
+
+#get users data: return all
+def get_city(con):
+    city_table = {}
+    curr = db.execute(" SELECT city_lon, city_lat FROM citydb")
+    tabs = curr.fetchall()
+    for rows in tabs:
+        city_table = rows
+    return city_table
     
-class fill_tables:
+class fill_tables_default:
     #fill tuples to user tables
     conn.execute("""INSERT INTO userdb(user_id,first_name,last_name) 
     VALUES
@@ -36,20 +59,23 @@ class fill_tables:
         (192387, 'Mark','Ronson')
         """)
     #fill tuples to city tables
-    conn.execute("""INSERT INTO citydb(city_name,city_desc)
+    conn.execute("""INSERT INTO citydb(city_name,city_desc,city_lon,city_lat)
         VALUES
-            ("Stockholm","Visit Stockholm is your guide to Stockholm and the Stockholm Archipelago. Get tips on restaurants, cafés, bars, shops, events, exhibitions, and activities."),
-            ("Munich", "Munich tourism: Get all the information on places to visit, what to see and things to do in Munich, here on the city's official website!")
+            ("Stockholm","Visit Stockholm is your guide to Stockholm and the Stockholm Archipelago. Get tips on restaurants, cafés, bars, shops, events, exhibitions, and activities.",59.3293,18.0686),
+            ("Munich", "Munich tourism: Get all the information on places to visit, what to see and things to do in Munich, here on the city's official website!",48.1351,11.5820)
         """)
     #fill tuples to trip tables
     # conn.execute("""INSERT INTO userdb(
         
     #     """)
-    
 
+    
 create_tables
-conn.commit()
+fill_tables_default
+db.commit()
 conn.close()   
+
+print(get_city(db))
     
 
 
